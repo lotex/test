caeef4927ac700e527113c785d8f81501e6b41b0
Saimir <5131804+lotex@users.noreply.github.com>
2025-03-16 18:44:38 -0400

    first commit

---
+++ sh-petclinic/src/test/java/com/shafizi/petclinic/ShPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shafizi.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
